52ef701d11a7d8dc0ea36aab7aafc12c5219e0f7
Will Moran <will.c.moran@gmail.com>
2024-05-09 14:14:51 -0400

    Added scripts and event listener to change technology in transientState

--- scripts/technologies.js
+++ scripts/technologies.js
@@ -1,8 +1,24 @@
+import { setTechnology } from "./transientState.js";
+
+// Define a function to change the wheelId in the transientState
+const handleTechnologyChange = (changeEvent) => {
+    // Check the id in the state of what was clicked
+    if (changeEvent.target.id === 'technologyDropdown') {
+        // Grab id from the state of what was changed and parseInt
+        const convertedToInteger = parseInt(changeEvent.target.value);
+        // Invoke function defined in the transientState module to change the paintId to the one we just parsed
+        setTechnology(convertedToInteger);
+    }
+}
+
 export const technologies = async () => {
     // Fetch data from db about technologies
     const fetchResponse = await fetch("http://localhost:8088/technology");
     // Convert data to JS object
     const technologies = await fetchResponse.json();
+
+    // Define an event listener that handles a change to technology selection    
+    document.addEventListener("change", handleTechnologyChange);
 
     // Begin building a dropdown
     let technologyHTML = `<select id="technologyDropdown">
